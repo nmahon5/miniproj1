4fb61ed75fef81ac44145e375464e0c8b3b50e34
nmahon5 <97564564+nmahon5@users.noreply.github.com>
2022-01-25 17:54:32 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
 function setup() {
-    createCanvas(800, 500);
-  }
+    createCanvas(windowWidth, windowHeight);
   
-  function draw() {
-    background(10,200,155);
-  }
+}
+
+
+
+
+
+  function draw() { 
+    background(255,104,100);
+    
+
+  line(0,0,windowWidth,windowHeight);
+
+  stroke(255,204,100)
+  line(windowWidth,0,0,windowHeight);
+
+
+
+
+    fill(50);
+    strokeWeight(5);
+    stroke(255,204,100);
+    circle(windowWidth/2,windowHeight/5,50);
+    
+    
+    fill(50);
+    strokeWeight(5);
+    stroke(10,10,255);
+    circle(windowWidth/5,windowHeight/2,50);
+    
+    
+   
+
+    }
